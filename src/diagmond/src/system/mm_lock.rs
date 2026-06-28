9a626c51fb531d739961074392bd9dd2aa5ef862
Marin <p1-mmr@users.noreply.github.com>
2026-06-28 19:50:38 +0200

    Add a provisional UI tab for displaying UDev rules (#10)

--- src/diagmond/src/system/mm_lock.rs
+++ src/diagmond/src/system/mm_lock.rs
@@ -14,6 +14,14 @@ pub async fn lock_device(connection: &Connection, device_name: &str) -> Result<(
     let proxy = ModemManagerProxy::new(connection).await?;
     proxy.inhibit_device(device_name, true).await?;
 
+    // TODO
+    // See: https://github.com/P1sec/QCSuper/blob/master/src/qcsuper/inputs/usb_modem_pyserial.py
+    // DROP INHIBITPROXY FOR NOW (BECAUSE OF https://gitlab.freedesktop.org/mobile-broadband/ModemManager/-/work_items/1075)
+    // AND USE UDEV RULES INSTEAD
+
+    // ^ WE NEED SOMETHING ELSE THAN THE DEVICE_UID FROM MODEMMANAGER
+    // TO CREATE A VALID UDEV RULE THEN ??
+
     Ok(())
 }
 
