3c72eee3e7bf1d3bc085f651a25dec23a629d7aa
Marin <p1-mmr@users.noreply.github.com>
2026-06-29 00:09:37 +0200

    Plan to use a ColumnView for the udev rules list

---
+++ src/diagng/system/udev_rules_dir.py
@@ -0,0 +1,4 @@
+# WIP - Cf. https://docs.gtk.org/gio/class.FileMonitor.html -
+#   https://github.com/P1sec/DiagNG/issues/10
+
+#   => https://lazka.github.io/pgi-docs/Gio-2.0/classes/File.html#Gio.File.monitor_directory
