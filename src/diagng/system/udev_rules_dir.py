1c6ffce3ef9d83206349d7648c2ccbabef6a978c
Marin <p1-mmr@users.noreply.github.com>
2026-06-29 10:28:05 +0200

    Add basic Gio file watch code

--- src/diagng/system/udev_rules_dir.py
+++ src/diagng/system/udev_rules_dir.py
@@ -1,14 +1,46 @@
+#!/usr/bin/env python3
+
+from gi.repository import GObject, Gio
+from logging import debug
+
 # WIP - Cf. https://docs.gtk.org/gio/class.FileMonitor.html -
 #   https://github.com/P1sec/DiagNG/issues/10
 
 #   => https://lazka.github.io/pgi-docs/Gio-2.0/classes/File.html#Gio.File.monitor_directory
 
+UDEV_RULES_DIR = '/run/udev/rules.d'
+
 
 class UdevRulesMonitor:
     app: 'MainApplication'
+
+    rules_dir: Gio.File
+    rules_dir_monitor: Gio.FileMonitor
 
     def __init__(self, app):
 
         self.app = app
 
-        pass  # WIP
+        self.rules_dir = Gio.File.new_for_path('/tmp/toto_dir')
+        self.rules_dir_monitor = self.rules_dir.monitor_directory(
+            Gio.FileMonitorFlags.NONE, None
+        )
+
+        self.rules_dir_monitor.connect('changed', self.dir_event)
+
+    def dir_event(
+        self,
+        file_monitor: Gio.FileMonitor,
+        fd: Gio.File,
+        other_file: Gio.File,
+        event_type: Gio.FileMonitorEvent,
+    ):
+        debug(
+            'UDev rules directory event received: '
+            + 'file=%r - other_file%r - event=%r'
+            % (
+                fd.get_path() if fd else None,
+                other_file.get_path() if other_file else None,
+                event_type.value_name,
+            )
+        )  # WIP
