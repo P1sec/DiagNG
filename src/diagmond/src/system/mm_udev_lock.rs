7cc1a93e88a1f0d9cbcc2be311681306a76debd8
Marin <p1-mmr@users.noreply.github.com>
2026-06-29 13:19:08 +0200

    Add plan for mm_udev_lock.rs

--- src/diagmond/src/system/mm_udev_lock.rs
+++ src/diagmond/src/system/mm_udev_lock.rs
@@ -1,8 +1,13 @@
 pub async fn lock_device(device_name: &str) -> zbus::Result<()> {
     log::error!("NOT IMPLEMENTED YET");
 
-    // TODO
+    // TODO - Implement this feature based of QCSuper logic
     // See: https://github.com/P1sec/QCSuper/blob/master/src/qcsuper/inputs/usb_modem_pyserial.py
+
+    // TODO: Add udev rule to "/run/udev/rules.d"
+    // TODO: Sync code contained in "udev_rules_dir.py"
+    // TODO: Reload udev rules (➡️ through which channel?)
+    // TODO: Restart ModemManager (➡️ through which channel?)
 
     Ok(())
 }
