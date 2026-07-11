ec8855df51223ef713a0df8307be96129f75570b
Marin <p1-mmr@users.noreply.github.com>
2026-07-11 12:05:12 +0200

    Plan more submodule files

--- src/diagng/system/adb_proxy.py
+++ src/diagng/system/adb_proxy.py
@@ -2,6 +2,6 @@
 
 # WIP 2026-07-11
 
-# ➡️ Write a GLib-based async callack-based wrapper
-# over the adb_client file for sending commands
+# ➡️ Write a GLib-based async callback-based wrapper
+# over the adb_client module for sending commands
 # from modules easily
