55239b5ade5440c6bf11827c273697ff0717792e
Marin <p1-mmr@users.noreply.github.com>
2026-07-13 12:26:19 +0200

    Plan the structure of into_gathering.py

---
+++ src/diagng/system/adb/adb_scripts/info_gathering.py
@@ -0,0 +1,11 @@
+# NEXT WIP : ➡️ ➡️ write a single Info Gathering script
+
+# TODO: Gather: getprop sys.usb.config diag,adb => current USB configuration
+
+# TODO: Gather: id => check root status
+
+# TODO: Gather: root status
+
+# TODO: Gather: su availability
+
+# etc.
