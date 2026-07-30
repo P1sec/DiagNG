689db92769c5c4a7fd77fe398072cf393d4c2bed
Marin <p1-mmr@users.noreply.github.com>
2026-07-30 11:05:39 +0200

    Prepare to implement dlf-to-pcap

--- src/diagng/protocol/qualcomm/acquisition/dlf_input.py
+++ src/diagng/protocol/qualcomm/acquisition/dlf_input.py
@@ -1,1 +1,6 @@
-TODO_XX
+#!/usr/bin/env python3
+
+from diagng.protocol.qualcomm.acquisition.base_input import BaseQCDMInput
+
+class DLFInput(BaseQCDMInput):
+    pass # TODO
---
+++ src/diagng/tools/dlf_to_pcap.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env
+from diagng.protocol.qualcomm.acquisition.dlf_input import DLFInput
+from diagng.protocol.qualcomm.struct.dlf_file import DlfFile
+
+from argparse import ArgumentParser
+
+def main():
+    pass # TODO
+
+if __name__ == '__main__':
+    main()
