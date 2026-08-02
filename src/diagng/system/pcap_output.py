afc11e8740d1a3e90f0a01beec8bb9e063055d10
Marin <p1-mmr@users.noreply.github.com>
2026-08-02 19:28:23 +0200

    Plan out pcap_output.py

---
+++ src/diagng/system/pcap_output.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+
+class PcapOutput:
+    pass  # TODO
--- src/diagng/tools/dlf_to_pcap.py
+++ src/diagng/tools/dlf_to_pcap.py
@@ -2,6 +2,8 @@
 from diagng.protocol.qualcomm.acquisition.dlf_input import DLFInput
 from diagng.protocol.qualcomm.struct.dlf_file import DlfFile
 
+from diagng.system.pcap_output import PcapOutput
+
 from argparse import ArgumentParser
 
 
