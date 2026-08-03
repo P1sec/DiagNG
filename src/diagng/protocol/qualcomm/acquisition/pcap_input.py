bde58742a259f8e549698833e3ce0a5b0287db99
Marin <p1-mmr@users.noreply.github.com>
2026-08-03 11:26:29 +0200

    Continue PCAP writer code framing

--- src/diagng/protocol/qualcomm/acquisition/pcap_input.py
+++ src/diagng/protocol/qualcomm/acquisition/pcap_input.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
 
 """
-    This class should be able to either spawn
-    a Wireshark subprocess (possibly bypassing
-    the Flatpak sandbox) and piping PCAP
-    output to it, or either writing this output
-    to a on-disk PCAP file.
+This class should be able to either spawn
+a Wireshark subprocess (possibly bypassing
+the Flatpak sandbox) and piping PCAP
+output to it, or either writing this output
+to a on-disk PCAP file.
 
-    Support for parsing an existing PCAP(ng) file for
-    reprocessing purposes
-        => Note: PCAPng is not
-        even supported by libpcap nor
-        by Kaitai and the simplest
-        way to add support seems to
-        be to use Tshark as an
-        external converted command
+Support for parsing an existing PCAP(ng) file for
+reprocessing purposes
+    => Note: PCAPng is not
+    even supported by libpcap nor
+    by Kaitai and the simplest
+    way to add support seems to
+    be to use Tshark as an
+    external converted command
 
-    (Should be also support reading a
-    PCAP file wrapped in a .GZ stream, like
-    in QCSuper? If so, should it also
-    involve a different wrapper class?)
+(Should be also support reading a
+PCAP file wrapped in a .GZ stream, like
+in QCSuper? If so, should it also
+involve a different wrapper class?)
 """
 
 
--- src/diagng/system/pcap_output.py
+++ src/diagng/system/pcap_output.py
@@ -1,32 +1,41 @@
 #!/usr/bin/python3
 
 """
-    This class should be able to either spawn
-    a Wireshark subprocess (possibly bypassing
-    the Flatpak sandbox) and piping PCAP
-    output to it, or either writing this output
-    to a on-disk PCAP file.
+This class should be able to either spawn
+a Wireshark subprocess (possibly bypassing
+the Flatpak sandbox) and piping PCAP
+output to it, or either writing this output
+to a on-disk PCAP file.
 
-    (Should be also support writing a
-    PCAP file wrapped in a .GZ stream, like
-    in QCSuper? If so, should it also
-    involve a different wrapper class?)
+(Should be also support writing a
+PCAP file wrapped in a .GZ stream, like
+in QCSuper? If so, should it also
+involve a different wrapper class?)
 
-                        => THIS SHALL BE INSTANCIED FROM: ⚠️ ⚠️ XX
-                        => HAVE METHODS BE CALLED FROM: ⚠️ ⚠️ XX    => ota_decoder.py ? ℹ️
-                        =>    AND IMPORT ;; => THE KAITAI PCAP+IP+UDP+GSMTAP encoder ?
-                            ( ⚠️ MOVE FUNCTION FROM OTA_DECODER FOR ENCODING A GSMTAP HEADER)
-                        =>    AND CALL; :  ⚠️ XX
+                    => THIS SHALL BE INSTANCIED FROM: ⚠️ ⚠️ XX
+                    => HAVE METHODS BE CALLED FROM: ⚠️ ⚠️ XX    => ota_decoder.py ? ℹ️
+                    =>    AND IMPORT ;; => THE KAITAI PCAP+IP+UDP+GSMTAP encoder ?
+                        ( ⚠️ MOVE FUNCTION FROM OTA_DECODER FOR ENCODING A GSMTAP HEADER)
+                    =>    AND CALL; :  ⚠️ XX
 
-    Cf. https://github.com/P1sec/QCSuper/blob/2.1.3/src/qcsuper/modules/pcap_dump.py
+
+            # See: ➡️ "adb_client.py" and "spawn_diagmond.py" for a
+            #  reference about how to INTERWORK with the Flatpak sandbox.
+
+Cf. https://github.com/P1sec/QCSuper/blob/2.1.3/src/qcsuper/modules/pcap_dump.py
 """
 
+
+IN_FLATPAK = XX
 
 class PcapOutput:
     XX: XX  # TODO (WIP)
 
-    def __init__(self, XX):
+    def __init__(self, use_wireshark=False, output_file: str | None = None):
         self.XX = XX
+
+    def spawn_wireshark(XX):
+        XX
 
     def write_gsmtap_frame(XX, XX, XX, XX):
         XX
