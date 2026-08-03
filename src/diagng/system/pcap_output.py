96d73cf06270947ed7cafff42e9be9a23115062f
Marin <p1-mmr@users.noreply.github.com>
2026-08-03 11:17:13 +0200

    Continue framing PCAP writer

--- src/diagng/system/pcap_output.py
+++ src/diagng/system/pcap_output.py
@@ -12,9 +12,24 @@
     in QCSuper? If so, should it also
     involve a different wrapper class?)
 
+                        => THIS SHALL BE INSTANCIED FROM: ⚠️ ⚠️ XX
+                        => HAVE METHODS BE CALLED FROM: ⚠️ ⚠️ XX    => ota_decoder.py ? ℹ️
+                        =>    AND IMPORT ;; => THE KAITAI PCAP+IP+UDP+GSMTAP encoder ?
+                            ( ⚠️ MOVE FUNCTION FROM OTA_DECODER FOR ENCODING A GSMTAP HEADER)
+                        =>    AND CALL; :  ⚠️ XX
+
     Cf. https://github.com/P1sec/QCSuper/blob/2.1.3/src/qcsuper/modules/pcap_dump.py
 """
 
 
 class PcapOutput:
-    pass  # TODO (WIP)
+    XX: XX  # TODO (WIP)
+
+    def __init__(self, XX):
+        self.XX = XX
+
+    def write_gsmtap_frame(XX, XX, XX, XX):
+        XX
+
+    def write_pcap_record(XX):
+        XX
