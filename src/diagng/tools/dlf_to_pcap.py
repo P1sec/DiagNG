24e543e62b061f74d8d5a096efc286ac3b01ccba
Marin <p1-mmr@users.noreply.github.com>
2026-08-08 08:01:04 +0200

    Add argument specification for dlf-to-pcap

--- src/diagng/tools/dlf_to_pcap.py
+++ src/diagng/tools/dlf_to_pcap.py
@@ -2,12 +2,21 @@
 from diagng.protocol.qualcomm.acquisition.dlf_input import DLFInput
 from diagng.protocol.qualcomm.struct.dlf_file import DlfFile
 
+from diagng.protocol.qualcomm.modules.ota_decoder import OTADecoder
 from diagng.system.pcap_output import PcapOutput
 
 from argparse import ArgumentParser
 
 
 def main():
+    args = ArgumentParser(description='Convert a .DLF file to a PCAP stream')
+    args.add_argument('input_dlf')
+    args.add_argument(
+        'output_pcap', nargs='?', help='Omit to open a Wireshark instance'
+    )
+
+    args = args.parse_args()
+
     pass  # TODO
 
 
