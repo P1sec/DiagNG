11534b60971667a7fd8f0715f89b746012f1ee1b
Marin <p1-mmr@users.noreply.github.com>
2026-08-10 09:06:49 +0200

    Extract timestamp from encapsulated DIAG_LOG_F packets when needed

--- src/diagng/system/pcap_output.py
+++ src/diagng/system/pcap_output.py
@@ -371,6 +371,11 @@ class PcapOutput(GObject.GObject):
 
         if isinstance(data, DiagLogF.InnerLog):
             self.write_ipv4_record(ipv4, data.unix_ts)
+        elif (
+            isinstance(data, DiagLogF.DiagResponse)
+            and data.cmd_code == DiagCmd.log_f
+        ):
+            self.write_ipv4_record(ipv4, data.payload.inner_log.unix_ts)
         else:
             self.write_ipv4_record(ipv4, time())
 
