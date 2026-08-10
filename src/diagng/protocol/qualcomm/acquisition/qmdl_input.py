1b12344f7d5b386d7d93753f62c63f4973a46aff
Marin <p1-mmr@users.noreply.github.com>
2026-08-10 07:38:16 +0200

    Add qmdl_input.py

---
+++ src/diagng/protocol/qualcomm/acquisition/qmdl_input.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+from diagng.protocol.qualcomm.acquisition.base_input import BaseQCDMInput
+from gzip import decompress
+from io import BytesIO
+
+
+class QMDLInput(BaseQCDMInput):
+    stream_io: BytesIO
+
+    def __init__(self, stream_io: BytesIO):
+        super().__init__()
+
+        try:
+            stream_io = BytesIO(decompress(stream_io.read()))
+        except Exception:
+            raise
+        stream_io.seek(0)
+
+        self.stream_io = stream_io
+
+    def process_stream(self):
+        self.process_input(self.stream_io.read())
+
+    def send_raw(self, data: bytes):
+        raise IOError('Stream is read-only')
+
+    def close(self):
+        self.closed.emit()
