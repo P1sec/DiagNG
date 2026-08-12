7e6b9f10a27f0af75c2d7904860a78c94e2f5bba
Marin <p1-mmr@users.noreply.github.com>
2026-08-12 23:51:29 +0200

    Add basic GSMTAP v3 header encoding definition

--- src/diagng/protocol/network/gsmtap_v3.py
+++ src/diagng/protocol/network/gsmtap_v3.py
@@ -3,6 +3,7 @@
 
 import kaitaistruct
 from kaitaistruct import ReadWriteKaitaiStruct, KaitaiStream, BytesIO
+from enum import IntEnum
 
 
 if getattr(kaitaistruct, 'API_VERSION', (0, 9)) < (0, 11):
@@ -13,20 +14,317 @@ if getattr(kaitaistruct, 'API_VERSION', (0, 9)) < (0, 11):
 
 
 class GsmtapV3(ReadWriteKaitaiStruct):
+    class Type(IntEnum):
+        osmocore_log = 0
+        sim = 1
+        baseband_diag = 2
+        signal_status_report = 3
+        tetra_i1 = 4
+        tetra_i1_burst = 5
+        gmr1_um = 6
+        e1t1 = 7
+        wmx_burst = 8
+        um = 512
+        um_burst = 513
+        gb_rlcmac = 514
+        gb_llc = 515
+        gb_sndcp = 516
+        abis = 517
+        rlp = 518
+        umts_mac = 768
+        umts_rlc = 769
+        umts_pdcp = 770
+        umts_rrc = 771
+        lte_mac = 1024
+        lte_rlc = 1025
+        lte_pdcp = 1026
+        lte_rrc = 1027
+        nas_eps = 1028
+        nr_mac = 1280
+        nr_rlc = 1281
+        nr_pdcp = 1282
+        nr_rrc = 1283
+        nas_5gs = 1284
+
     def __init__(self, _io=None, _parent=None, _root=None):
         super(GsmtapV3, self).__init__(_io)
         self._parent = _parent
         self._root = _root or self
 
     def _read(self):
-        pass
+        self.reserved = self._io.read_u1()
+        if not self.reserved == 0:
+            raise kaitaistruct.ValidationNotEqualError(
+                0, self.reserved, self._io, '/seq/0'
+            )
+        self.header_len = self._io.read_u2be()
+        if not self.header_len >= 2:
+            raise kaitaistruct.ValidationLessThanError(
+                2, self.header_len, self._io, '/seq/1'
+            )
+        self.type = KaitaiStream.resolve_enum(
+            GsmtapV3.Type, self._io.read_u2be()
+        )
+        _on = self.type
+        if _on == GsmtapV3.Type.nr_rrc:
+            pass
+            self.subtype = GsmtapV3.NrRrcSubtype(self._io, self, self._root)
+            self.subtype._read()
+        else:
+            pass
+            self.subtype = self._io.read_u2be()
+        self.metadata = []
+        i = 0
+        while True:
+            _t_metadata = GsmtapV3.Metadata(self._io, self, self._root)
+            try:
+                _t_metadata._read()
+            finally:
+                _ = _t_metadata
+                self.metadata.append(_)
+            if _.tag == GsmtapV3.Metadata.Tag.end_of_metadata:
+                break
+            i += 1
+        self.data = self._io.read_bytes_full()
         self._dirty = False
 
     def _fetch_instances(self):
         pass
+        _on = self.type
+        if _on == GsmtapV3.Type.nr_rrc:
+            pass
+            self.subtype._fetch_instances()
+        else:
+            pass
+        for i in range(len(self.metadata)):
+            pass
+            self.metadata[i]._fetch_instances()
 
     def _write__seq(self, io=None):
         super(GsmtapV3, self)._write__seq(io)
+        self._io.write_u1(self.reserved)
+        self._io.write_u2be(self.header_len)
+        self._io.write_u2be(int(self.type))
+        _on = self.type
+        if _on == GsmtapV3.Type.nr_rrc:
+            pass
+            self.subtype._write__seq(self._io)
+        else:
+            pass
+            self._io.write_u2be(self.subtype)
+        for i in range(len(self.metadata)):
+            pass
+            self.metadata[i]._write__seq(self._io)
+
+        self._io.write_bytes(self.data)
+        if not self._io.is_eof():
+            raise kaitaistruct.ConsistencyError(
+                'data', 0, self._io.size() - self._io.pos()
+            )
 
     def _check(self):
+        if not self.reserved == 0:
+            raise kaitaistruct.ValidationNotEqualError(
+                0, self.reserved, None, '/seq/0'
+            )
+        if not self.header_len >= 2:
+            raise kaitaistruct.ValidationLessThanError(
+                2, self.header_len, None, '/seq/1'
+            )
+        _on = self.type
+        if _on == GsmtapV3.Type.nr_rrc:
+            pass
+            if self.subtype._root != self._root:
+                raise kaitaistruct.ConsistencyError(
+                    'subtype', self._root, self.subtype._root
+                )
+            if self.subtype._parent != self:
+                raise kaitaistruct.ConsistencyError(
+                    'subtype', self, self.subtype._parent
+                )
+        else:
+            pass
+        if len(self.metadata) == 0:
+            raise kaitaistruct.ConsistencyError(
+                'metadata', 0, len(self.metadata)
+            )
+        for i in range(len(self.metadata)):
+            pass
+            if self.metadata[i]._root != self._root:
+                raise kaitaistruct.ConsistencyError(
+                    'metadata', self._root, self.metadata[i]._root
+                )
+            if self.metadata[i]._parent != self:
+                raise kaitaistruct.ConsistencyError(
+                    'metadata', self, self.metadata[i]._parent
+                )
+            _ = self.metadata[i]
+            if (_.tag == GsmtapV3.Metadata.Tag.end_of_metadata) != (
+                i == len(self.metadata) - 1
+            ):
+                raise kaitaistruct.ConsistencyError(
+                    'metadata',
+                    i == len(self.metadata) - 1,
+                    _.tag == GsmtapV3.Metadata.Tag.end_of_metadata,
+                )
+
         self._dirty = False
+
+    class Metadata(ReadWriteKaitaiStruct):
+        class Tag(IntEnum):
+            packet_timestamp = 0
+            packet_comment = 1
+            channel_number = 2
+            frequency = 3
+            band_indicator = 4
+            bsic_psc_pci = 5
+            gsm_timeslot = 6
+            gsm_subslot = 7
+            system_frame_number = 8
+            subframe_number = 9
+            hyperframe_number = 10
+            tetra_symbol_number = 11
+            tetra_multiframe_number = 12
+            antenna_number = 13
+            signal_level = 256
+            rssi = 257
+            snr = 258
+            sinr = 259
+            rscp = 260
+            ecio = 261
+            rsrp = 262
+            rsrq = 263
+            ss_rsrp = 264
+            csi_rsrp = 265
+            srs_rsrp = 266
+            ss_rsrq = 267
+            csi_rsrq = 268
+            ss_sinr = 269
+            csi_sinr = 270
+            ciphering_key = 512
+            integrity_key = 513
+            k_nasenc = 514
+            k_nasint = 515
+            k_rrcenc = 516
+            k_rrcint = 517
+            k_upenc = 518
+            k_upint = 519
+            end_of_metadata = 65534
+
+        def __init__(self, _io=None, _parent=None, _root=None):
+            super(GsmtapV3.Metadata, self).__init__(_io)
+            self._parent = _parent
+            self._root = _root
+
+        def _read(self):
+            self.tag = KaitaiStream.resolve_enum(
+                GsmtapV3.Metadata.Tag, self._io.read_u2be()
+            )
+            if self.tag != GsmtapV3.Metadata.Tag.end_of_metadata:
+                pass
+                self.len_value = self._io.read_u2be()
+
+            if self.tag != GsmtapV3.Metadata.Tag.end_of_metadata:
+                pass
+                self.value = self._io.read_bytes(self.len_value)
+
+            self._dirty = False
+
+        def _fetch_instances(self):
+            pass
+            if self.tag != GsmtapV3.Metadata.Tag.end_of_metadata:
+                pass
+
+            if self.tag != GsmtapV3.Metadata.Tag.end_of_metadata:
+                pass
+
+        def _write__seq(self, io=None):
+            super(GsmtapV3.Metadata, self)._write__seq(io)
+            self._io.write_u2be(int(self.tag))
+            if self.tag != GsmtapV3.Metadata.Tag.end_of_metadata:
+                pass
+                self._io.write_u2be(self.len_value)
+
+            if self.tag != GsmtapV3.Metadata.Tag.end_of_metadata:
+                pass
+                self._io.write_bytes(self.value)
+
+        def _check(self):
+            if self.tag != GsmtapV3.Metadata.Tag.end_of_metadata:
+                pass
+
+            if self.tag != GsmtapV3.Metadata.Tag.end_of_metadata:
+                pass
+                if len(self.value) != self.len_value:
+                    raise kaitaistruct.ConsistencyError(
+                        'value', self.len_value, len(self.value)
+                    )
+
+            self._dirty = False
+
+    class NrRrcSubtype(ReadWriteKaitaiStruct):
+        class Subtype(IntEnum):
+            bcch_bch = 1
+            bcch_dl_sch = 2
+            dl_ccch = 3
+            dl_dcch = 4
+            mcch = 5
+            pcch = 6
+            ul_ccch = 7
+            ul_ccch1 = 8
+            ul_dcch = 9
+            sbcch_sl_bch = 257
+            scch = 258
+            rrc_reconfiguration = 513
+            rrc_reconfiguration_complete = 514
+            ue_mrdc_capability = 515
+            ue_nr_capability = 516
+            ue_radio_access_capability_information = 517
+            ue_radio_paging_information = 518
+            sib1 = 519
+            sib2 = 520
+            sib3 = 521
+            sib4 = 522
+            sib5 = 523
+            sib6 = 524
+            sib7 = 525
+            sib8 = 526
+            sib9 = 527
+            sib10_r16 = 528
+            sib11_r16 = 529
+            sib12_r16 = 530
+            sib13_r16 = 531
+            sib14_r16 = 532
+            sib15_r17 = 533
+            sib16_r17 = 534
+            sib17_r17 = 535
+            sib18_r17 = 536
+            sib19_r17 = 537
+            sib20_r17 = 538
+            sib21_r17 = 539
+            sib22_r18 = 540
+            sib23_r18 = 541
+            sib24_r18 = 542
+            sib25_r18 = 543
+            sib17bis_r18 = 544
+
+        def __init__(self, _io=None, _parent=None, _root=None):
+            super(GsmtapV3.NrRrcSubtype, self).__init__(_io)
+            self._parent = _parent
+            self._root = _root
+
+        def _read(self):
+            self._unnamed0 = KaitaiStream.resolve_enum(
+                GsmtapV3.NrRrcSubtype.Subtype, self._io.read_u2be()
+            )
+            self._dirty = False
+
+        def _fetch_instances(self):
+            pass
+
+        def _write__seq(self, io=None):
+            super(GsmtapV3.NrRrcSubtype, self)._write__seq(io)
+            self._io.write_u2be(int(self._unnamed0))
+
+        def _check(self):
+            self._dirty = False
--- struct/gsmtap/gsmtap_v3.ksy
+++ struct/gsmtap/gsmtap_v3.ksy
@@ -5,6 +5,203 @@ meta:
   bit-endian: be
 
 # From: https://gitea.osmocom.org/peremen/gsmtapv3/src/branch/master/GSMTAPv3.md
+# From: https://gitea.osmocom.org/peremen/gsmtapv3/src/branch/master/include/osmocom/core/gsmtapv3.h
 
-# seq:
+seq:
+  - id: reserved
+    type: u1
+    valid:
+      eq: 0
+
+  - id: header_len
+    type: u2
+    valid:
+      min: 2
+
+  - id: type
+    type: u2
+    enum: type
+
+  - id: subtype
+    type:
+      switch-on: type
+      cases:
+        'type::nr_rrc': nr_rrc_subtype
+        _: u2
+
+  - id: metadata
+    type: metadata
+    repeat: until
+    repeat-until: _.tag == metadata::tag::end_of_metadata
+
+  - id: data
+    size-eos: true
+
+#   (WIP)
+
+enums:
+  type:
+    # 0x00, 0x01: Common and non-3GPP protocols
+
+    0x0000: osmocore_log # libosmocore logging
+    0x0001: sim # ISO 7816 smartcard interface
+    0x0002: baseband_diag # Baseband diagnostic data
+    0x0003: signal_status_report # Radio signal status report
+    0x0004: tetra_i1 # TETRA air interface
+    0x0005: tetra_i1_burst # TETRA air interface
+    0x0006: gmr1_um # GMR-1 L2 packets
+    0x0007: e1t1 # E1/T1 Lines
+    0x0008: wmx_burst # WiMAX burst
+
+    # 0x02: GSM
+
+    0x0200: um
+    0x0201: um_burst # raw burst bits
+    0x0202: gb_rlcmac # GPRS Gb interface: RLC/MAC
+    0x0203: gb_llc # GPRS Gb interface: LLC
+    0x0204: gb_sndcp # GPRS Gb interface: SNDCP
+    0x0205: abis
+    0x0206: rlp # GSM RLP frames, as per 3GPP TS 24.022
+
+    # 0x03: UMTS/WCDMA
+
+    0x0300: umts_mac # UMTS MAC PDU with context, as per 3GPP TS 25.321
+    0x0301: umts_rlc # UMTS RLC PDU with context, as per 3GPP TS 25.322
+    0x0302: umts_pdcp # UMTS PDCP PDU with context, as per 3GPP TS 25.323
+    0x0303: umts_rrc # UMTS RRC PDU, as per 3GPP TS 25.331
+
+    # 0x04: LTE
+
+    0x0400: lte_mac # LTE MAC PDU with context, as per 3GPP TS 36.321
+    0x0401: lte_rlc # LTE RLC PDU with context, as per 3GPP TS 36.322
+    0x0402: lte_pdcp # LTE PDCP PDU with context, as per 3GPP TS 36.323
+    0x0403: lte_rrc # LTE RRC PDU, as per 3GPP TS 36.331
+    0x0404: nas_eps # EPS Non-Access Stratum, as per 3GPP TS 24.301
+
+    # 0x05: NR
+
+    0x0500: nr_mac # NR MAC PDU with context, as per 3GPP TS 38.321
+    0x0501: nr_rlc # NR RLC PDU with context, as per 3GPP TS 38.322
+    0x0502: nr_pdcp # NR PDCP PDU with context, as per 3GPP TS 38.323
+    0x0503: nr_rrc # NR RRC PDU, as per 3GPP TS 38.331
+    0x0504: nas_5gs # 5GS Non-Access Stratum, as per 3GPP TS 24.501
+
+types:
+  metadata:
+    seq:
+      - id: tag
+        type: u2
+        enum: tag
+
+      - if: tag != tag::end_of_metadata
+        id: len_value
+        type: u2
+
+      - if: tag != tag::end_of_metadata
+        id: value
+        size: len_value
+
+    enums:
+      tag:
+        # GSMTAPv3 metadata tags
+        0x0000: packet_timestamp
+        0x0001: packet_comment
+        0x0002: channel_number
+        0x0003: frequency
+        0x0004: band_indicator
+        0x0005: bsic_psc_pci
+        0x0006: gsm_timeslot
+        0x0007: gsm_subslot
+        0x0008: system_frame_number
+        0x0009: subframe_number
+        0x000a: hyperframe_number
+        0x000b: tetra_symbol_number
+        0x000c: tetra_multiframe_number
+        0x000d: antenna_number
+
+        0x0100: signal_level
+        0x0101: rssi
+        0x0102: snr
+        0x0103: sinr
+        0x0104: rscp
+        0x0105: ecio
+        0x0106: rsrp
+        0x0107: rsrq
+        0x0108: ss_rsrp
+        0x0109: csi_rsrp
+        0x010a: srs_rsrp
+        0x010b: ss_rsrq
+        0x010c: csi_rsrq
+        0x010d: ss_sinr
+        0x010e: csi_sinr
+
+        0x0200: ciphering_key
+        0x0201: integrity_key
+        0x0202: k_nasenc
+        0x0203: k_nasint
+        0x0204: k_rrcenc
+        0x0205: k_rrcint
+        0x0206: k_upenc
+        0x0207: k_upint
+
+        0xfffe: end_of_metadata
+
+
+  nr_rrc_subtype:
+    seq:
+      - type: u2
+        enum: subtype
+
+    enums:
+      subtype:
+        # sub-types for TYPE_NR_RRC (0x0503)
+
+        0x0001: bcch_bch
+        0x0002: bcch_dl_sch
+        0x0003: dl_ccch
+        0x0004: dl_dcch
+        0x0005: mcch
+        0x0006: pcch
+        0x0007: ul_ccch
+        0x0008: ul_ccch1
+        0x0009: ul_dcch
+
+        0x0101: sbcch_sl_bch
+        0x0102: scch
+
+        # sub-types for individual NR RRC message
+
+        0x0201: rrc_reconfiguration
+        0x0202: rrc_reconfiguration_complete
+        0x0203: ue_mrdc_capability
+        0x0204: ue_nr_capability
+        0x0205: ue_radio_access_capability_information
+        0x0206: ue_radio_paging_information
+        0x0207: sib1
+        0x0208: sib2
+        0x0209: sib3
+        0x020a: sib4
+        0x020b: sib5
+        0x020c: sib6
+        0x020d: sib7
+        0x020e: sib8
+        0x020f: sib9
+        0x0210: sib10_r16
+        0x0211: sib11_r16
+        0x0212: sib12_r16
+        0x0213: sib13_r16
+        0x0214: sib14_r16
+        0x0215: sib15_r17
+        0x0216: sib16_r17
+        0x0217: sib17_r17
+        0x0218: sib18_r17
+        0x0219: sib19_r17
+        0x021a: sib20_r17
+        0x021b: sib21_r17
+        0x021c: sib22_r18
+        0x021d: sib23_r18
+        0x021e: sib24_r18
+        0x021f: sib25_r18
+        0x0220: sib17bis_r18
+
 #   (WIP)
