f304b58ba3d63bcc97dca0af4c9e081993c70208
Marin <p1-mmr@users.noreply.github.com>
2026-09-24 07:21:01 +0200

    Drop Flathub-related requirements as we're going to self-host the repository

--- packaging/flatpak/build.sh
+++ packaging/flatpak/build.sh
@@ -6,18 +6,12 @@ set -ex
 # Move to the application's root
 cd "$(dirname "$0")/../.."
 
-# Store offline sources for dependencies (required to build on Flathub)
-cd diagmond/
-mkdir -p .cargo
-cargo vendor --locked vendor | sed 's/^directory = ".*"/directory = "vendor"/g' > .cargo/config.toml
-cd ..
-
 # Install the required Flatpak runtime and SDK
 flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub --user org.gnome.Sdk//50 -y
-flatpak install flathub --user org.gnome.Platform//50 -y
+flatpak install flathub --user org.gnome.Sdk//51 -y
+flatpak install flathub --user org.gnome.Platform//51 -y
 
-# Build the Flathub package
+# Build the Flatpak
 rm -rf diagmond/target/ # Don't copy all the planet into the Flatpak build dir
 rm -rf repo/
 PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig/ flatpak-builder --install repo packaging/flatpak/com.p1security.diagng.json --user -y
